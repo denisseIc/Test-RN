399b35d21912aa73efc8b8956eef2797cf1ac8e2
denisse23 <denisse@icoms.co>
2021-09-06 14:05:03 -0600

    Second commit - don't remember

--- android/app/src/main/java/com/iconic/calculadorarn/MainApplication.java
+++ android/app/src/main/java/com/iconic/calculadorarn/MainApplication.java
@@ -1,4 +1,5 @@
-package com.calculadora;
+//package com.calculadora;
+package com.iconic.calculadorarn;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
@@ -65,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.calculadora.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.iconic.calculadorarn.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
